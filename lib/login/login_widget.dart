0f99599494029be3ad67ad7e28661f75af33d47a
Elie <eliezigani2@gmail.com>
2025-06-24 23:05:45 +0000

    retrait condition de connexion en fonction du type utilisateur et correction de version

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -1,18 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
 import '/backend/api_requests/api_calls.dart';
+import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_drop_down.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/form_field_controller.dart';
-import '/backend/schema/structs/index.dart';
 import '/index.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
 import 'login_model.dart';
+
 export 'login_model.dart';
 
 class LoginWidget extends StatefulWidget {
@@ -43,30 +45,18 @@ class _LoginWidgetState extends State<LoginWidget>
       setDarkModeSetting(context, ThemeMode.system);
       FFAppState().pageconnexion = 'connexion';
       safeSetState(() {});
+
       if (FFAppState().authtoken != '') {
-        if (FFAppState().type == 'CLIENT') {
-          context.goNamed(
-            HomeWidget.routeName,
-            extra: <String, dynamic>{
-              kTransitionInfoKey: TransitionInfo(
-                hasTransition: true,
-                transitionType: PageTransitionType.fade,
-                duration: Duration(milliseconds: 0),
-              ),
-            },
-          );
-        } else {
-          context.goNamed(
-            AgentWidget.routeName,
-            extra: <String, dynamic>{
-              kTransitionInfoKey: TransitionInfo(
-                hasTransition: true,
-                transitionType: PageTransitionType.fade,
-                duration: Duration(milliseconds: 0),
-              ),
-            },
-          );
-        }
+        context.goNamed(
+          HomeWidget.routeName,
+          extra: <String, dynamic>{
+            kTransitionInfoKey: TransitionInfo(
+              hasTransition: true,
+              transitionType: PageTransitionType.fade,
+              duration: Duration(milliseconds: 0),
+            ),
+          },
+        );
       } else {
         context.goNamed(LoginWidget.routeName);
       }
